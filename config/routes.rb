f389273184bbe187fec2e71588c62eedf2d7384c
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-03-11 18:59:52 -0500

    Adds sessions controller, user model and controller, migrations for user model and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  poat '/login' => 'sessions#create'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
