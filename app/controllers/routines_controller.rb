34e6d5164a005f738279f26a414fb9d57bfdc0d0
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-03-12 19:16:08 -0500

    added ability for user to create a new routine

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -3,4 +3,19 @@ class RoutinesController < ApplicationController
     def new
         @routine = Routine.new
     end
+
+    def create
+        @routine = Routine.new(user_params)
+        if @routine.save
+            redirect_to routines_path
+        else
+            render :new
+        end
+    end
+
+    private
+    #strong params
+    def user_params
+        params.require(:routine).permit(:day)
+    end
 end
