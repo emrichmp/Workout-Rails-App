0297d3923edfab9269fa2348f2689929f6d99f89
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-03-22 22:57:28 -0400

    established relationships between comment, user and routine

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,2 +1,5 @@
 class Routine < ApplicationRecord
+    belongs_to :user
+    has_many :comments
+    has_many :users, through: :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+    has_many :routines
+    has_many :comments
+    has_many :commented_routines, through: :comments,
+        source: :routine
+
     has_secure_password
 
     validates :username, presence: true
