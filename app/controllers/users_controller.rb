1eb04e4a985991ee4f52fc4659127f3a08b3a748
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-03-24 15:31:43 -0400

    n/a

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,6 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             session[:user_id] = @user.id
-            #change this
             redirect_to routines_path
         else
             render :new
