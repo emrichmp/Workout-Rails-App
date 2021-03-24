f2e6d5073893aba3f19d17397f14bfb5f48709ce
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-03-24 00:55:33 -0400

    added ability for users to make and view comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,10 @@
 class CommentsController < ApplicationController
     before_action :redirect_if_not_logged_in
 
+    def index
+        @comments = Comment.all
+    end
+
     def new
         @comment = Comment.new
     end
