21f02445bf497045c67353d0d3631a16405d9d34
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-03-23 23:35:13 -0400

    updated spec.md, added validations to models

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -2,4 +2,5 @@ class Routine < ApplicationRecord
     belongs_to :user
     has_many :comments
     has_many :users, through: :comments
+    validates :day, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
     has_secure_password
 
-    validates :username, presence: true
     validates :username, uniqueness: true
+    validates :username, :password, presence: true
 end
