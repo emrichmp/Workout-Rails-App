a2f454ebabb7420fb0c1046efb5d4deb2919ab1e
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-03-24 23:56:45 -0400

    added custom validation

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -4,7 +4,18 @@ class Routine < ApplicationRecord
     has_many :users, through: :comments
 
     validates :day, presence: true
+    #limits the amount of routine instances a user can make a day
+    validate :limit_routines
     
     #scope method
     scope :alpha, -> { order(:day) }
+
+    def limit_routines
+        today_comments = user.comments.select do |i|
+            i.created_at.to_date == Date.today
+        end
+        if today_comments.size > 2
+            errors.add(:routine_id, "- Can't create more than 3 routines per day")
+        end
+    end
 end
