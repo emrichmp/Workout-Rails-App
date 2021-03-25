5b2ac32174e5c73cc67374dc56cd7feea069fdd3
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-03-25 00:10:36 -0400

    cleaned up

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -36,16 +36,4 @@ class CommentsController < ApplicationController
     def comment_params
         params.require(:comment).permit(:content,:routine_id)
       end
-    
-      def set_comment
-        @comment = Comment.find_by(id: params[:id])
-        # if !@comment
-        #   flash[:message] = "Comment was not found"
-        #   redirect_to comments_path
-        # end
-      end
-    
-      def redirect_if_not_comment_author
-         redirect_to comments_path if @comment.user != current_user
-      end
 end
