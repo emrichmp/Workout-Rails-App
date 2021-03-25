c973229096c9a03a6dbafe0e6b3f564b30502bd6
Emrich-Michael Perrier <emrichmp@gmail.com>
2021-03-24 23:41:31 -0400

    added partials for error messages

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -2,6 +2,7 @@ class Routine < ApplicationRecord
     belongs_to :user
     has_many :comments
     has_many :users, through: :comments
+
     validates :day, presence: true
     
     #scope method
